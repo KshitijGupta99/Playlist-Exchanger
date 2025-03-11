499f31775241d5e243fb0a841690578e8d7763ea
KshitijGupta99 <guptakshitij111@gmail.com>
2025-03-11 16:22:49 +0530

    initialize backend and frontend with React, TypeScript, and Vite setup

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api', (req, res) => {
+  res.send('Hello API! hi there everyone')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
