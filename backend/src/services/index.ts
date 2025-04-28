1aa52270d03af8015437d163d3f431d6d5ba006f
KshitijGupta99 <guptakshitij111@gmail.com>
2025-04-28 20:11:43 +0530

    chore: Remove unnecessary newline in service index file

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -1,5 +1,4 @@
 import SpotifyService from "./spotify.services";
 import YoutubeService from "./youtube.services";
 import ConverterService from "./converter.services";
-
 export { SpotifyService, YoutubeService, ConverterService };
